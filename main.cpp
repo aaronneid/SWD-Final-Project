9d93c0c051325724bf8deb85974e5376927ca75b
nicpadilla <nic.padilla96@gmail.com>
2017-04-21 21:02:06 -0500

    Update/Remove Order - Creates backup

--- main.cpp
+++ main.cpp
@@ -21,8 +21,9 @@ class Admin{
 		Admin();
 		~Admin();
 		void trackOrder(string orderNum);
+                void updateOrder(string orderNum);
 	protected:
-		void updateOrder();
+		
 	private:
 		void addCustomer();
 		void addOrder();
@@ -56,6 +57,75 @@ void Admin::trackOrder(string orderNum){
 	}
 	
 	fileIn.close();
+}
+
+void Admin::updateOrder(string orderNum) {
+	string line, temp, new_location, cur_location, dest_location, status;
+	vector<string> line_info;
+	
+	int found, line_num = -1, action = 0; //action: remove(0), update(1)
+	ifstream file("Orders.txt");
+        ofstream backup("Backup.txt");
+        fstream fileOut("Orders.txt");
+        
+	if(file.fail()){
+		cout << "No order update could be performed! File does not exist!" << endl;
+		exit(0);
+	}
+
+	while(1) {
+            string input;
+	cout << "What action would you like to perform?" <<
+		"\n[0] Remove Order" << "\n[1] Update Order Location" << endl;
+            getline(cin, input);
+            stringstream stream(input);
+            if(stream >> action && (action == 1 || action == 0)) break;
+            cout << "Invalid selection, try again." << endl << endl;
+        }
+	
+        if(action == 1) { //if update, store new location
+            cout << "Enter the current location: " << endl;
+            getline(cin, new_location);
+        }
+              
+        
+	//read file into vector
+	for(int i = 0; getline(file, temp); i++) {
+            line_info.push_back(temp);
+	}
+        
+        //copy file (backup)
+        for(auto &i : line_info) {
+            backup << i << endl;
+        }
+        for(int i = 0; i < line_info.size(); i++) {
+            stringstream data_stream(line_info[i]);
+            getline(data_stream, temp, ' ');
+            if(temp == orderNum) { //if we store the line number, we can read 3 times more to get the current location
+                line_num = i;
+                getline(data_stream, cur_location, ' ');
+                getline(data_stream, cur_location, ' '); //stores current location
+                getline(data_stream, dest_location, ' '); //stores destination location
+                getline(data_stream, temp, ' ');
+                getline(data_stream, status, ' ');      //stores status
+            }
+            if(action == 0 && line_num != -1) { //remove case
+                line_info[i].erase(line_info[i].begin()+line_num);
+            }
+            if(action == 1 && line_num != -1) { //update case
+                line_info[i].replace(line_info[i].find(cur_location), cur_location.length(), new_location);
+                if(new_location == dest_location) line_info[i].replace(line_info[i].find(status), status.length(), "complete");
+                break;
+            }
+        }
+        //cout << "Line Number: " << line_num << endl;
+        //write data back out to file
+        for(auto &i : line_info) {
+            fileOut << i << endl;
+        }
+        file.close();
+        backup.close();
+        fileOut.close();
 }
 
 Admin::~Admin(){
@@ -92,7 +162,7 @@ Customer::~Customer(){
 }
  
  int main(){
-	int test;
+	int test, selection = 0;
 	
 	Admin adminTester;
 	Courier courierTester;
@@ -118,6 +188,10 @@ Customer::~Customer(){
 	customerTester.trackOrder("12345");
 	
 	
+	while(selection != 0) {
+		
+	}
+	adminTester.updateOrder("1234545");
  }
  
 int login(string name, string password){
