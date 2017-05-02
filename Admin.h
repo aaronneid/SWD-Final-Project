84106bbdad50f630623b002665ff9ad6d69e36f4
nicpadilla <hotelmariofan@gmail.com>
2017-05-02 12:51:39 -0500

    Functioning updateOrder

--- Admin.h
+++ Admin.h
@@ -1,4 +1,9 @@
 #include"Miscellaneous.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <limits>
 
 #ifndef __Admin_h_INCLUDED__
 #define __Admin_h_INCLUDED__
@@ -6,7 +11,7 @@
 using namespace std;
 
 class Admin{
-	private:
+	public:
 		void addCustomer();
 		void addOrder();
 		void viewActiveCustomers();
@@ -68,72 +73,78 @@ void Admin::trackOrder(string orderNum){
 }
 
 void Admin::updateOrder(string orderNum) {
-	string line, temp, new_location, cur_location, dest_location, status;
-	vector<string> line_info;
+	string line, new_location, sTemp, input;
+	int action;
+	vector<string> order;
 	
-	int found, line_num = -1, action = 0; //action: remove(0), update(1)
-	ifstream file("Orders.txt");
-        ofstream backup("Backup.txt");
-        fstream fileOut("Orders.txt");
+        vector<string> new_order;
         
-	if(file.fail()){
+	ifstream fileIn("Orders.txt");
+	ofstream fileOut("Backup.txt");
+	
+	if(fileIn.fail()){
 		cout << "No order update could be performed! File does not exist!" << endl;
 		exit(0);
 	}
-
-	while(1) {
-            string input;
-			ClearScreen();
-	cout << "What action would you like to perform?" << "\nOrder Number: " << orderNum <<
-		"\n[0] Remove Order" << "\n[1] Update Order Location" << endl;
-            getline(cin, input);
-            stringstream stream(input);
-            if(stream >> action && (action == 1 || action == 0)) break;
-            cout << "Invalid selection, try again." << endl << endl;
-        }
 	
-        if(action == 1) { //if update, store new location
+	//each order in entry in vector 'order'
+	while(getline(fileIn, line)) {
+            //cout << "test";
+                stringstream inputStream;
+		inputStream << line;
+                vector<string> vTemp;
+		while(getline(inputStream, sTemp, '|')) {
+			vTemp.push_back(sTemp);
+                        //cout << vTemp[0] << endl;
+                }
+		if((vTemp[0].compare(orderNum)) == 0) {
+                    new_order = vTemp;
+                } //found order, put in separate vector
+                else order.push_back(line); //if order not found, put this order in order vector
+	}
+	if(new_order.size() == 0) {
+		cout << "Order not found." << endl; 
+		return;
+	}
+	//select remove or update order location
+	while(1) {
+		cout << "What action would you like to perform?" <<
+			"\n[0] Remove Order" << "\n[1] Update Order Location" << endl;
+		cin >> action;
+		cin.ignore();
+		if((action != 1 && action != 0)) {
+                    cout << "Invalid selection, try again." << endl << endl;
+		}
+		else break;
+	}
+	
+	if(action == 0) {
+		for(int k = 0; k < order.size(); k++) {
+                    fileOut << order[k];
+                    if(k != order.size()-1)
+                        fileOut << '\n';
+		}
+	}
+	
+	if(action == 1) {
+            //if update, store new location
             cout << "Enter the current location: " << endl;
             getline(cin, new_location);
-        }
-              
-        
-	//read file into vector
-	for(int i = 0; getline(file, temp); i++) {
-            line_info.push_back(temp);
+		new_order[2] = new_location;
+		if(new_order[2].compare(new_order[3]) == 0) new_order[5] = "Delivered";
+		
+		for(int k = 0; k < order.size(); k++) {
+                    fileOut << order[k];
+                    if(k != order.size()) fileOut << '\n';
+		}
+		//Write modified order back to output file
+		for(auto &k : new_order) {
+			fileOut << k;
+			fileOut << '|';
+		}
 	}
-        
-        //copy file (backup)
-        for(auto &i : line_info) {
-            backup << i << endl;
-        }
-        for(int i = 0; i < line_info.size(); i++) {
-            stringstream data_stream(line_info[i]);
-            getline(data_stream, temp, ' ');
-            if(temp == orderNum) { //if we store the line number, we can read 3 times more to get the current location
-                line_num = i;
-                getline(data_stream, cur_location, ' ');
-                getline(data_stream, cur_location, ' '); //stores current location
-                getline(data_stream, dest_location, ' '); //stores destination location
-                getline(data_stream, temp, ' ');
-                getline(data_stream, status, ' ');      //stores status
-            }
-            if(action == 0 && line_num != -1) { //remove case
-                line_info[i].erase(line_info[i].begin()+line_num);
-            }
-            if(action == 1 && line_num != -1) { //update case
-                line_info[i].replace(line_info[i].find(cur_location), cur_location.length(), new_location);
-                if(new_location == dest_location) line_info[i].replace(line_info[i].find(status), status.length(), "complete");
-                break;
-            }
-        }
-        //cout << "Line Number: " << line_num << endl;
-        //write data back out to file
-        for(auto &i : line_info) {
-            fileOut << i << endl;
-        }
-        file.close();
-        backup.close();
+	
+	fileIn.close();
         fileOut.close();
 }
 
